494e8a3a8d5e0bc4fd9384546571929ec832d550
IPL1987 <ihorpoznyak@yandex.com>
2023-07-06 22:14:47 +0300

    fix router

--- 007-middleware/api.js
+++ 007-middleware/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const logger = require('./middleware/logger');
-const route = require('./routes/bookRoute');
+const router = require('./routes/bookRoute');
 const error = require('./middleware/error-404');
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/books', route);
+app.use('/api/books', router);
 app.use(logger);
 app.use(error);
 
