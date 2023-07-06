547807bc46340eceb9d48aa24f11796770393909
IPL1987 <ihorpoznyak@yandex.com>
2023-07-06 22:15:36 +0300

    fix Book & stor

--- 007-middleware/Book/book.js
+++ 007-middleware/Book/book.js
@@ -2,45 +2,23 @@ const { v4: uuid } = require('uuid')
 
 class Book {
   constructor(
-    title = 'title',
-    desc = 'description',
-    authors = 'authors',
-    favorite = 'favorite',
-    fileCover = 'fileCover',
-    fileName = 'fileName',
-    fileBook = 'fileBook',
-    id = uuid()
-    ) {
+    title = "",
+    description = "",
+    authors = "",
+    favorite = "",
+    fileCover = "",
+    fileName = "",
+    fileBook = ""
+  ) {
+    this.id = uuid();
     this.title = title;
-    this.desc = desc;
+    this.description = description;
     this.authors = authors;
     this.favorite = favorite;
     this.fileCover = fileCover;
     this.fileName = fileName;
-    this.fileBook = fileBook;
-    this.id = id;
+    this.fileBook = fileBook
   }
 }
 
-const stor = {
-  books: [
-    new Book(
-      'Книга 1',
-      'Преступление и наказание',
-      'Ф.М. Достоевский',
-    ),
-    new Book(
-      'Книга 2',
-      'Идиот',
-      'Ф.М. Достоевский',
-    ),
-    new Book(
-      'Книга 3',
-      'Сон смешного человека',
-      'Ф.М. Достоевский',
-    ),
-  ],
-};
-
 module.exports = Book;
-module.exports = stor;
