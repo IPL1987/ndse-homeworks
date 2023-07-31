deff01a478b6dc96e9b39a766c31aade82b2479e
IPL1987 <ihorpoznyak@yandex.com>
2023-07-31 12:59:54 +0300

    fix

--- 009-docker-2/routes/bookRoute.js
+++ 009-docker-2/routes/bookRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const fileMulter = require('../middleware/file');
 const Book = require('../Book/book')
-const counter = require('./counterReq')
 
 const stor = {
   books: [
